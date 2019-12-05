7c31776c77073500ea2bb852370eeed8705800f7
anxoret <post.lux@yandex.ru>
2019-12-05 19:09:11 +0400

    Creation of header + beginning of creation slideshow

---
+++ js/es6/slideshow.js
@@ -0,0 +1,20 @@
+"use strict";
+
+let slides = document.querySelectorAll(".slideshow__slide");
+let showingSlide = 0;
+
+const showNextSlide = () => {
+    slides[showingSlide].classList.remove("slideshow__slide_show");
+    
+    if (showingSlide == slides.length - 1) {
+        showingSlide = 0;
+    } else {
+        showingSlide++;
+    }
+
+    slides[showingSlide].classList.add("slideshow__slide_show");
+    
+};
+
+let slideInterval = setInterval(showNextSlide, 3000);
+
